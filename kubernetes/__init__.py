070c522a75bb3d531553360efd0fcfca1853f34a
Austin Macdonald <austin@redhat.com>
2022-08-18 18:17:49 -0400

    generated client change

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "24.2.0"
+__version__ = "24.2.1"
 
 import kubernetes.client
 import kubernetes.config
